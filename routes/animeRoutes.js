b6e1a45928cf4a5b8fbf189304bdef7547e429bd
Isaiah Gonzalez <isaiah@Isaiahs-MacBook-Pro-2.local>
2018-11-26 15:18:51 -0500

    update

--- routes/animeRoutes.js
+++ routes/animeRoutes.js
@@ -5,16 +5,26 @@ const Anime = require("../models/Anime");
 
 
 router.get("/anime", (req, res, next)=>{
-  axios.get("https://kitsu.io/api/edge/anime/")
-  .then((infoFromAPI)=>{
-    console.log(infoFromAPI.data.results);
-   res.render('Anime/animeList', {anime: infoFromAPI.data})
+  axios.get("https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0")
+  .then((infoFromAPI)=>{ 
+    // console.log(('=-=-=-=-=-=-=-=-=-=',infoFromAPI.data.data[0]));
+   res.render('Anime/animeList', {anime: infoFromAPI.data.data})
   })
   .catch((err)=>{
     next(err)
   })
 })
 
+router.get('/anime/:id', (req, res, next)=>{
+  axios.get(`https://kitsu.io/api/edge/anime/${req.params.id}`)
+  .then((response)=>{
+    
+    res.render('Anime/animeDetails', {oneAnime: response.data.data})
+  })
+  .catch((err)=>{
+    next(err)
+  })
+})
 
 
 
