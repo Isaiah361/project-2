988ab2a4dcc594c5db634e0de7ecab66bf5d099f
Isaiah Gonzalez <isaiah@Isaiahs-MacBook-Pro-2.local>
2018-11-28 15:03:35 -0500

    update

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const flash         = require("connect-flash")
 const User          = require('./models/User');
 
 mongoose
-  .connect('mongodb://localhost/anime', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+ .connect(process.env.MONGODB_URI, {useMongoClient: true})
+ .then(() => {
+   console.log('Connected to Mongo!')
+ }).catch(err => {
+   console.error('Error connecting to mongo', err)
+ });
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
